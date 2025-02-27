7dbf0dfa6e1d78a11b52a67c9d0c2143d714afe4
unknown <alejandraviles2005@gmail.com>
2025-02-26 20:36:30 -0600

    Se realizó el diccionario.

---
+++ pruebas_fase2.jl
@@ -0,0 +1,31 @@
+# Alejandra Avilés - 24722
+# Materia: Algoritmos y estructuras de datos
+# Tarea: Investigación de Lenguajes ----- Fase 2
+
+# Prueba 1: Demostración del uso de un diccionario en Julia
+
+pronombres_en_frances = Dict(
+    "yo" => "je",
+    "tú" => "tu",
+    "él/ella/usted" => "il/elle",
+    "nosotros/as" => "nous",
+    "vosotros/as" => "vous",
+)
+
+# Iteración sobre el diccionario
+
+println("**********Prueba de diccionario**********")
+println("los pronombres del español se traducen así en francés: ")
+for(pronombre, traduccion) in pronombres_en_frances # Aplicación de un bucle
+     println("$pronombre se traduce en francés como: $traduccion.")
+end
+println()
+
+println("Para decir 'yo' en francés es: ", pronombres_en_frances["yo"]) # Acceso al valor de una clave
+println()
+
+pronombres_en_frances["ustedes"] = "vous" # Agregar una clave y valor al inicio del diccionario
+println(pronombres_en_frances) # Imprimir el contenido del diccionario actualizado
+println()
+println()
+# Fin de la prueba 1
