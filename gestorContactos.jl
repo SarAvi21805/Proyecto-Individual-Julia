d7486fee8c843a2e1914e04cc8f5b110e50741b5
unknown <alejandraviles2005@gmail.com>
2025-04-18 17:24:41 -0600

    Se trabajó en la implementación de nuevos métodos que incluyen: manejo de contactos con CSV; eliminación, modificación y búsqueda parcial de un contacto.

--- gestorContactos.jl
+++ gestorContactos.jl
@@ -4,6 +4,10 @@
 # Descripción: Programa que permitirá al usuario agregar, buscar y listar contactos guardados.
 # Fecha de última modificación: 18/04/2025
 
+#importando recursos necesarios
+using CSV
+using DataFrames
+
 # Estructura para los contactos
 struct Contacto
     nombre::String
@@ -42,6 +46,55 @@ function listar_contactos()
         println("Nombre: teléfono")
         for (nombre, contacto) in contactos
             println("$nombre: $(contacto.telefono)")
+        end
+    end
+end
+
+# Función que permite cargar los contactos desde un archivo CSV
+function cargar_contactos_archivo_csv(archivo::String)
+    if isfile(archivo)
+        df = CSV.File(archivo) |> DataFrames
+        for row in eachrow(df)
+            contactos[row.nombre] = Contacto(row.nombre, row.telefono)
+        end
+    end
+end
+
+# Función para guardar contactos en el CSV
+function guardar_contactos(archivo::String)
+    df = DataFrame(nombre=String[], telefono=String[])
+    for contacto in values(contactos)
+        push!(df, (contacto.nombre, contacto.telefono))
+    end
+    CSV.write(archivo, df)
+end
+
+# Función para eliminar contactos
+function eliminar_contacto(nombre::String)
+    if haskey(contactos, nombre)
+        delete!(contactos, nombre)
+        println("Contacto $nombre eliminado con éxito.")
+    else
+        println("Contacto $nombre no encontrado.")
+    end
+end
+
+# Función para modificar contactos
+function modificar_contacto(nombre::String, nuevo_telefono::String)
+    if haskey(contactos, nombre)
+        contactos[nombre].telefono = nuevo_telefono
+        println("Contacto modificado: $nombre")
+    else
+        println("Contacto $nombre no encontrado.")
+    end
+end
+
+#Función para efectuar búsquedas de contactos por número de teléfono parciales
+function buscar_por_telefono(telefono::String)
+    println("Resultados de búsqueda para el número: $telefono")
+    for contacto in values(contactos)
+        if eccursin(telefono, contacto.telefono)
+            println("Nombre: $(contacto.nombre), Teléfono: $(contacto.telefono)")
         end
     end
 end
