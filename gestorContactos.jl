165c47c7779d7bcdf5e77c39344ff1fb7a1982c6
unknown <alejandraviles2005@gmail.com>
2025-04-24 19:22:13 -0600

    Se optimizó la opción para modificar contactos mediante la validación de la cantidad de dígitos en el número de teléfono.

--- gestorContactos.jl
+++ gestorContactos.jl
@@ -30,13 +30,11 @@ function agregar_contacto(nombre::String, telefono::String)
     end
 end
 
-
+# Función que permite la búsqueda parcial de contactos mediante el nombre
 function buscar_contacto(lista::Vector{String}, nombre::String)
     # Conversión del texto a minúsculas
     texto_a_buscar_lower = lowercase(nombre)
-
     coincidencias = filter(x -> occursin(texto_a_buscar_lower, lowercase(x)), lista)
-
     return coincidencias
 end
 
@@ -85,8 +83,18 @@ end
 # Función para modificar contactos
 function modificar_contacto(nombre::String, nuevo_telefono::String)
     if haskey(contactos, nombre)
-        contactos[nombre].telefono = nuevo_telefono
-        println("Contacto modificado: $nombre")
+        nuevo_telefono = ""
+        
+        while true
+            nuevo_telefono = readLine("Ingrese el nuevo teléfono (8 dígitos): ")
+            if validar_telefono(nuevo_telefono)
+                contactos[nombre].telefono = nuevo_telefono
+                println("Contacto modificado: $nombre")
+                break
+            else
+                println("Número de teléfono inválido. Debe contener 8 dígitos.")
+            end
+        end
     else
         println("Contacto $nombre no encontrado.")
     end
@@ -102,11 +110,16 @@ function buscar_por_telefono(telefono::String)
     end
 end
 
+# Función para validar el número de teléfono
+function validar_telefono(telefono::String)
+    return length(telefono) == 8 && all(isdigit, telefono)
+end
+
 # Menú interactivo
 function menu()
     cargar_contactos("contactos.csv") # Carga inicial de datos
     while true
-        println("\n***** Menú de Contactos *****")
+        println("\n***** Menú de Contactos *****") # Menú interactivo
         println("1. Agregar contacto.")
         println("2. Modificar contacto.")
         println("3. Eliminar contacto.")
@@ -117,21 +130,21 @@ function menu()
         print("Seleccione una opción: ")
         opcion = parse(Int, readline())
 
-        if opcion == 1
+        if opcion == 1 # Agregar contacto
             print("Ingrese el nombre: ")
             nombre = readline()
             print("Ingrese el teléfono: ")
             telefono = readline()
             agregar_contacto(nombre, telefono)
             guardar_contactos("contactos.csv")
-        elseif opcion == 2
+        elseif opcion == 2 # Modificar contacto
             print("Ingrese el nombre del contacto a modificar: ")
             nombre = readline()
             print("Ingrese el nuevo teléfono: ")
             nuevo_telefono = readline()
             modificar_contacto(nombre, nuevo_telefono)
             guardar_contactos("contactos.csv")
-        elseif opcion == 3
+        elseif opcion == 3 # Eliminar contacto
             print("Ingrese el nombre del contacto a eliminar: ")
             nombre = readline()
             println("¿Está seguro que desea eliminar el contacto $nombre? (s/n)")
@@ -140,7 +153,7 @@ function menu()
                 eliminar_contacto(nombre)
             end
             guardar_contactos("contactos.csv")
-        elseif opcion == 4
+        elseif opcion == 4 # Buscar contacto por nombre
             print("Ingrese el nombre del contacto a buscar: ")
             nombre = readline()
             #buscar_contacto(nombre)
@@ -154,17 +167,17 @@ function menu()
                     println("- $nombre_coincidencia: $(contacto.telefono)")
                 end
             end
-        elseif opcion == 5
+        elseif opcion == 5 # Buscar contacto por teléfono
             print("Ingrese el número de teléfono a buscar: ")
             telefono = readline()
             buscar_por_telefono(telefono)
-        elseif opcion == 6
+        elseif opcion == 6 # Listar contactos
             listar_contactos()
-        elseif opcion == 7
+        elseif opcion == 7 # Salir
             guardar_contactos("contactos.csv") # Guarda los contactos previamente al Salir
             println("Saliendo del programa...")
             break
-        else
+        else # Manejo de errores
             println("Opción inválida. Por favor, seleccione una opción válida.")
         end
     end
